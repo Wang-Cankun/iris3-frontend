2c790fc659a545c856199b126e8abb0c4290f795
Cankun Wang <flykun0620@gmail.com>
2020-09-03 18:34:00 -0400

    Add register function

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -4,13 +4,19 @@
       <v-flex xs12 class="text-xs-center" mt-5>
         <h1 class="text-center">Sign up</h1>
       </v-flex>
-      <v-flex xs12 sm6 offset-sm3 mt-3>
+      <v-flex xs12 sm4 offset-sm4 mt-3>
         <v-alert v-if="error" prominent type="error" dismissible>
           <v-row align="center">
             <v-col class="grow">Error code {{ statusCode }}: {{ error }}</v-col>
           </v-row>
         </v-alert>
-        <v-form method="post" @submit.prevent="register">
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          method="post"
+          @submit.prevent="register"
+        >
           <v-layout column>
             <v-flex>
               <v-text-field
@@ -31,6 +37,7 @@
                 label="Password"
                 type="password"
                 required
+                :rules="passwordRules"
               ></v-text-field>
             </v-flex>
             <v-flex>
@@ -39,8 +46,56 @@
                 name="confirmPassword"
                 label="Confirm Password"
                 type="password"
+                :rules="confirmPasswordRules"
                 required
               ></v-text-field>
+            </v-flex>
+            <v-flex>
+              <v-text-field
+                id="firstName"
+                v-model="firstName"
+                name="firstName"
+                label="First Name"
+                type="string"
+                :rules="nameRules"
+                required
+              ></v-text-field>
+              <v-text-field
+                id="lastName"
+                v-model="lastName"
+                name="lastName"
+                label="Last Name"
+                type="string"
+                :rules="nameRules"
+                required
+              ></v-text-field>
+              <v-text-field
+                id="institution"
+                v-model="institution"
+                name="institution"
+                label="Institution"
+                type="string"
+                :rules="nameRules"
+                required
+              ></v-text-field>
+              <v-checkbox v-model="newsletter">
+                <template v-slot:label>
+                  <div>
+                    Admin Email Delivery.
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-icon color="primary" dark v-on="on"
+                          >mdi-help-circle-outline</v-icon
+                        >
+                      </template>
+                      By checking this option, you would like to receive
+                      information emails from IRIS3 adminsitrators (new feature
+                      announcements, unplanned site maintenance, general
+                      notices, etc.)..
+                    </v-tooltip>
+                  </div>
+                </template>
+              </v-checkbox>
             </v-flex>
             <v-flex class="text-xs-center" mt-5>
               <v-btn color="primary" type="submit">Sign Up</v-btn>
@@ -59,28 +114,60 @@
 <script>
 export default {
   middleware: 'guest',
-  data: () => ({
-    username: '',
-    email: '',
-    password: '',
-    statusCode: '',
-    error: null,
-    emailRules: [
-      (v) => (/.+@.+\..+/.test(v) && v.length > 0) || 'E-mail must be valid',
-    ],
-  }),
+  data() {
+    return {
+      valid: true,
+      username: '',
+      email: '',
+      password: '',
+      firstName: '',
+      lastName: '',
+      institution: '',
+      newsletter: false,
+      confirmPassword: '',
+      statusCode: '',
+      error: null,
+      emailRules: [
+        (v) => (/.+@.+\..+/.test(v) && v.length > 0) || 'E-mail must be valid',
+      ],
+      passwordRules: [
+        (value) => !!value || 'Please type password.',
+        (value) => (value && value.length >= 6) || 'minimum 6 characters',
+      ],
+      confirmPasswordRules: [
+        (value) => !!value || 'Please type confirm password',
+        (value) =>
+          value === this.password ||
+          'The password confirmation does not match.',
+      ],
+      nameRules: [
+        (v) => !!v || 'Field is required',
+        (v) => (v && v.length <= 30) || 'Field must be less than 30 characters',
+        (v) => (v && v.length > 1) || 'Field must be more than 1 characters',
+      ],
+    }
+  },
   methods: {
+    validate() {
+      this.$refs.form.validate()
+    },
     async register() {
+      if (!this.$refs.form.validate()) return
       try {
-        await this.$axios.post('register', {
+        await this.$axios.post('users', {
           username: this.username,
           email: this.email,
           password: this.password,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          institution: this.institution,
+          newsletter: this.newsletter,
+          job: [],
         })
 
         await this.$auth.loginWith('local', {
           data: {
-            email: this.email,
+            username: this.email,
             password: this.password,
           },
         })
