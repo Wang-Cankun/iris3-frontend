b69b91d023684c3b40910331b2489cecad7384c6
Cankun Wang <flykun0620@gmail.com>
2020-10-29 22:50:57 -0400

    Add vue drag resize function

---
+++ components/utils/ResizeImage.vue
@@ -0,0 +1,47 @@
+<template>
+  <vdr
+    :w="500"
+    :h="500"
+    :is-conflict-check="true"
+    :parent="true"
+    @dragging="onDrag"
+    @resizing="onResize"
+  >
+    <v-card class="mx-auto"
+      ><v-card-title class="primary white--text subtitle-1"
+        >{{ title }} </v-card-title
+      ><img :src="src" :width="width" :height="height"
+    /></v-card>
+  </vdr>
+</template>
+
+<script>
+export default {
+  props: {
+    src: { type: String, required: true },
+    title: { type: String, required: true },
+  },
+  data() {
+    return {
+      width: 500,
+      height: 500,
+      x: 50,
+      y: 70,
+    }
+  },
+  methods: {
+    onResize(x, y, width, height) {
+      this.x = x
+      this.y = y
+      this.width = width
+      this.height = height
+    },
+    onDrag(x, y) {
+      this.x = x
+      this.y = y
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- plugins/plotly.js
+++ plugins/plotly.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import vPlotly from '@statnett/vue-plotly'
+import vdr from 'vue-draggable-resizable-gorkys'
+
+import 'vue-draggable-resizable-gorkys/dist/VueDraggableResizable.css'
 
 const VuePlotly = {
   install(Vue) {
@@ -9,4 +12,5 @@ const VuePlotly = {
 Vue.use(VuePlotly)
 Vue.component('downloadExcel')
 
+Vue.component('vdr', vdr)
 export default { VuePlotly }
