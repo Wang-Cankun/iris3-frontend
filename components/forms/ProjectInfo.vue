e6463764a7df0e52eb9563809702aced4f9569c9
Cankun Wang <flykun0620@gmail.com>
2020-11-05 19:23:59 -0500

    Update Resize function

--- components/forms/ProjectInfo.vue
+++ components/forms/ProjectInfo.vue
@@ -63,8 +63,8 @@ export default {
     return {
       projectTitle: 'Example title1',
       projectDescription: 'Example description1',
-      projectCreateDate: '10/29/2020',
-      jobCreateDate: '10/29/2020',
+      projectCreateDate: '11/05/2020',
+      jobCreateDate: '11/05/2020',
       projectCreator: 'Admin',
       jobCreator: 'Admin',
       projectId: 'P000001',
--- components/utils/ResizeImage.vue
+++ components/utils/ResizeImage.vue
@@ -1,16 +1,37 @@
 <template>
   <vdr
-    :w="500"
-    :h="500"
-    :is-conflict-check="true"
+    :w="width"
+    :h="height"
+    :x="x"
+    :y="y"
+    :min-width="400"
+    :min-height="400"
+    :grid="[100, 100]"
+    :is-conflict-check="false"
     :parent="true"
     @dragging="onDrag"
     @resizing="onResize"
   >
     <v-card class="mx-auto"
-      ><v-card-title class="primary white--text subtitle-1"
-        >{{ title }} </v-card-title
-      ><img :src="src" :width="width" :height="height"
+      ><v-card-title ref="titleBox" class="primary white--text subtitle-1"
+        >{{ title }} <v-spacer></v-spacer>
+        <v-menu bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item @click="downloadPNG">
+              <v-list-item-title>Download PNG</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="downloadPDF">
+              <v-list-item-title>Download PDF</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu></v-card-title
+      ><img :src="src" :width="width" :height="height - titleHeight"
     /></v-card>
   </vdr>
 </template>
@@ -20,16 +41,23 @@ export default {
   props: {
     src: { type: String, required: true },
     title: { type: String, required: true },
+    x: { type: Number, required: true },
+    y: { type: Number, required: true },
   },
   data() {
     return {
       width: 500,
       height: 500,
-      x: 50,
-      y: 70,
+      titleHeight: 60,
     }
   },
+  mounted() {
+    this.matchHeight()
+  },
   methods: {
+    matchHeight() {
+      this.titleHeight = this.$refs.titleBox.clientHeight + 15
+    },
     onResize(x, y, width, height) {
       this.x = x
       this.y = y
@@ -39,6 +67,16 @@ export default {
     onDrag(x, y) {
       this.x = x
       this.y = y
+    },
+    downloadPNG() {
+      const link = document.createElement('a')
+      link.href = this.src
+      link.setAttribute('download', 'Image_png.png')
+      document.body.appendChild(link)
+      link.click()
+    },
+    downloadPDF() {
+      return 1
     },
   },
 }
