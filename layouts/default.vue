a4335bb8665c72a26c0bd0866b2c15dc01e72135
Alex1516612806 <70406534+Alex1516612806@users.noreply.github.com>
2021-02-17 23:36:10 -0500

    Page change

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,9 +19,7 @@
       <v-toolbar-items class="hidden-xs-only mx-1">
         <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" elevation="0" v-on="on">
-              Explore
-            </v-btn>
+            <v-btn v-bind="attrs" elevation="0" v-on="on"> Explore </v-btn>
           </template>
           <v-list>
             <v-list-item
@@ -37,9 +35,7 @@
 
         <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" elevation="0" v-on="on">
-              Help
-            </v-btn>
+            <v-btn v-bind="attrs" elevation="0" v-on="on"> Help </v-btn>
           </template>
           <v-list>
             <v-list-item
@@ -53,11 +49,9 @@
           </v-list>
         </v-menu>
 
-        <v-menu open-on-hover offset-y>
+        <!-- <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" elevation="0" v-on="on">
-              News
-            </v-btn>
+            <v-btn v-bind="attrs" elevation="0" v-on="on"> News </v-btn>
           </template>
           <v-list>
             <v-list-item
@@ -69,13 +63,11 @@
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
           </v-list>
-        </v-menu>
+        </v-menu> -->
 
         <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" elevation="0" v-on="on">
-              Development
-            </v-btn>
+            <v-btn v-bind="attrs" elevation="0" v-on="on"> Development </v-btn>
           </template>
           <v-list>
             <v-list-item
@@ -91,9 +83,7 @@
 
         <v-menu open-on-hover offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" elevation="0" v-on="on">
-              About
-            </v-btn>
+            <v-btn v-bind="attrs" elevation="0" v-on="on"> About</v-btn>
           </template>
           <v-list>
             <v-list-item
@@ -107,6 +97,7 @@
           </v-list>
         </v-menu>
       </v-toolbar-items>
+
       <v-spacer></v-spacer>
       <v-text-field
         v-model="searchJobId"
@@ -151,98 +142,121 @@
         </template>
       </v-toolbar-items>
     </v-app-bar>
+
+    <v-carousel show-arrows-on-hover height="200">
+      <v-carousel-item>
+        <p>this is news</p>
+      </v-carousel-item>
+    </v-carousel>
+
     <v-main>
       <nuxt />
     </v-main>
-    <v-footer
-      color="primary"
-      app
-      inset
-      absolute
-      class="font-weight-light"
-      width="auto"
-      padless
-    >
-      <v-row class="mx-16">
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">EXPLORE</p>
-          <p v-for="item in exploreMenu" :key="item.title">
-            <nuxt-link
-              :to="item.path"
-              style="color: white; text-decoration: none;"
-            >
-              {{ item.title }}</nuxt-link
-            >
-          </p>
-        </v-col>
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">HELP</p>
-          <p v-for="item in helpmenu" :key="item.title">
-            <nuxt-link
-              :to="item.path"
-              style="color: white; text-decoration: none;"
-            >
-              {{ item.title }}</nuxt-link
-            >
-          </p>
-          <p><v-spacer></v-spacer></p>
-        </v-col>
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">NEWS</p>
-          <p v-for="item in newsmenu" :key="item.title">
-            <nuxt-link
-              :to="item.path"
-              style="color: white; text-decoration: none;"
-            >
-              {{ item.title }}</nuxt-link
-            >
-          </p>
-        </v-col>
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">DEVELOPMENT</p>
-          <p v-for="item in developmenu" :key="item.title">
-            <nuxt-link
-              :to="item.path"
-              style="color: white; text-decoration: none;"
-            >
-              {{ item.title }}</nuxt-link
-            >
-          </p>
-        </v-col>
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">ABOUT</p>
-          <p v-for="item in aboutmenu" :key="item.title">
-            <nuxt-link
-              :to="item.path"
-              style="color: white; text-decoration: none;"
-            >
-              {{ item.title }}</nuxt-link
-            >
-          </p>
-        </v-col>
-        <v-col col="2">
-          <p class="font-weight-bold" style="color: white;">SOCIAL</p>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon dark left>
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
-      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-        IRIS3 (v1.2.2) is developed by
-        <a href="https://u.osu.edu/bmbl/">BMBL</a>, it is free and open to all
-        users. | {{ new Date().getFullYear() }}
-      </v-col>
-      <login-dialog
-        :dialog.sync="loginDialog"
-        @close="loginDialog = false"
-      ></login-dialog>
-      <register-dialog
-        :dialog.sync="registerDialog"
-        @close="registerDialog = false"
-      ></register-dialog>
-    </v-footer>
+
+    <!--footer start here-->
+    <div>
+      <v-card>
+        <v-footer
+          color="primary"
+          app
+          inset
+          absolute
+          class="font-weight-light"
+          width="70%"
+          padless
+        >
+          <v-row class="mx-16">
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">EXPLORE</p>
+              <p v-for="item in exploreMenu" :key="item.title">
+                <nuxt-link
+                  :to="item.path"
+                  style="color: white; text-decoration: none; font-size: 12px;"
+                >
+                  {{ item.title }}</nuxt-link
+                >
+              </p>
+            </v-col>
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">HELP</p>
+              <p v-for="item in helpmenu" :key="item.title">
+                <nuxt-link
+                  :to="item.path"
+                  style="color: white; text-decoration: none; font-size: 12px;"
+                >
+                  {{ item.title }}</nuxt-link
+                >
+              </p>
+              <p><v-spacer></v-spacer></p>
+            </v-col>
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">NEW</p>
+              <p v-for="item in newsmenu" :key="item.title">
+                <nuxt-link
+                  :to="item.path"
+                  style="color: white; text-decoration: none; font-size: 12px;"
+                >
+                  {{ item.title }}</nuxt-link
+                >
+              </p>
+            </v-col>
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">DEVELOPMENT</p>
+              <p v-for="item in developmenu" :key="item.title">
+                <nuxt-link
+                  :to="item.path"
+                  style="color: white; text-decoration: none; font-size: 12px;"
+                >
+                  {{ item.title }}</nuxt-link
+                >
+              </p>
+            </v-col>
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">ABOUT</p>
+              <p v-for="item in aboutmenu" :key="item.title">
+                <nuxt-link
+                  :to="item.path"
+                  style="color: white; text-decoration: none; font-size: 12px;"
+                >
+                  {{ item.title }}</nuxt-link
+                >
+              </p>
+            </v-col>
+            <v-col col="1">
+              <p class="font-weight-bold" style="color: white;">SOCIAL</p>
+              <v-btn
+                v-for="icon in icons"
+                :key="icon"
+                class="mx-4"
+                icon
+                dark
+                left
+              >
+                <v-icon size="24px">
+                  {{ icon }}
+                </v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text rounded-b-xl"
+            cols="12"
+          >
+            IRIS3 (v1.2.2) is developed by
+            <a href="https://u.osu.edu/bmbl/">BMBL</a>, it is free and open to
+            all users. | {{ new Date().getFullYear() }}
+          </v-col>
+          <login-dialog
+            :dialog.sync="loginDialog"
+            @close="loginDialog = false"
+          ></login-dialog>
+          <register-dialog
+            :dialog.sync="registerDialog"
+            @close="registerDialog = false"
+          ></register-dialog>
+        </v-footer>
+      </v-card>
+    </div>
     <snackbar></snackbar>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,26 @@
 <template>
-  <v-container>
-    <v-row class="my-14">
-      <!-- Header Text -->
-      <v-row>
-        <p class="text-h4 text-center">
-          <strong>IRIS3</strong>: Integrated Cell-type-specific Regulon
-          Inference Server from Single-cell sequencing data
-        </p>
-      </v-row>
-    </v-row>
-
-    <!-- three buttons -->
-    <v-row class="my-12" justify="space-between">
-      <v-btn to="/upload" color="primary" width="240" rounded x-large>
-        new project
-      </v-btn>
-      <v-btn to="/public" color="primary" width="240" rounded x-large>
-        Public projects
-      </v-btn>
-      <v-btn to="/tutorial" color="primary" width="240" rounded x-large>
-        Tutorial
-      </v-btn>
-    </v-row>
-
-    <v-row class="my-6">
-      <v-col>
-        <!-- Display window  -->
-        <v-carousel
-          cycle
-          hide-delimiter-background
-          show-arrows-on-hover
-          height="auto"
-        >
-          <v-carousel-item>
-            <v-img max-width="500" src="img/abstract_workflow.jpg"></v-img>
-          </v-carousel-item>
-        </v-carousel>
-      </v-col>
+  <v-container width="100">
+    <!-- Header Text -->
+    <v-parallax height="1000" src="img/RNA_Seq_lead.jpg" class="animate-pop-in">
+      <h1 class="text-h1 text-center">
+        <strong>IRIS3</strong>
+      </h1>
+      <v-card-title>
+        <div class="typewriter center">
+          <h2 class="text-center">
+            Integrated Cell-type-specific Regulon Inference Server from
+            Single-cell sequencing data
+          </h2>
+        </div>
+      </v-card-title>
 
       <!-- Login  -->
-      <v-col>
-        <v-card class="mb-8" color="#BBDEFB" height="290" outlined>
-          <v-card-title>
-            Sign In
-          </v-card-title>
+      <v-col col="2">
+        <v-card class="px-12" color="#BBDEFB" height="290" outlined>
+          <v-card-title>Sign In</v-card-title>
           <v-text-field
             v-model="email"
-            class="mx-6"
+            class="mx-12"
             label="Email"
             outlined
           ></v-text-field>
@@ -62,53 +36,84 @@
             <v-btn rounded>Create an account</v-btn>
           </v-row>
         </v-card>
-
-        <!-- news  -->
-        <v-card height="220" outlined>
-          <!-- THe most updated news add here -->
-          <v-card-title class="mb-4">
-            News
-          </v-card-title>
-
-          <v-card-subtitle>
-            <p class="text-h8">
-              <strong>{{ newsdate }}</strong> {{ newscontent }}
-            </p>
-          </v-card-subtitle>
-          <v-card-actions>
-            <v-btn outlined rounded to="/news">
-              More+
-            </v-btn>
-          </v-card-actions>
-        </v-card>
       </v-col>
-    </v-row>
 
-    <!-- Public projects  -->
-    <v-row class="my-6">
-      <v-col lg="12">
-        <v-card outlined height="220" rounded="lg">
+      <!-- Three buttons -->
+      <v-row class="my-12" justify="space-between">
+        <v-btn to="/upload" color="primary" width="240" rounded x-large>
+          new project
+        </v-btn>
+        <v-btn to="/public" color="primary" width="240" rounded x-large>
+          Public projects
+        </v-btn>
+        <v-btn to="/tutorial" color="primary" width="240" rounded x-large>
+          Tutorial
+        </v-btn>
+      </v-row>
+    </v-parallax>
+
+    <!-- news  -->
+    <!-- THe most updated news add here -->
+    <!-- <v-card height="220" outlined>
+      <v-card-title class="mb-4">
+        News
+      </v-card-title>
+
+      <v-card-subtitle>
+        <p class="text-h8">
+          <strong>{{ newsdate }}</strong> {{ newscontent }}
+        </p>
+      </v-card-subtitle>
+      <v-card-actions>
+        <v-btn outlined rounded to="/news">
+          More+
+        </v-btn>
+      </v-card-actions>
+    </v-card> -->
+    <!-- </v-col> -->
+    <!-- </v-row> -->
+
+    <v-divider></v-divider>
+
+    <v-timeline :dense="$vuetify.breakpoint.smAndDown">
+      <v-timeline-item>
+        <!-- Start  -->
+        <span slot="opposite"> From us </span>
+        <v-card outlined height="220" rounded="lg" class="pa-3">
           <v-card-title class="justify-center"
             >Explore public IRIS3 projects
           </v-card-title>
           <v-card-subtitle>
             <p class="text-h7">
-              <strong> Project 1: </strong> <v-spacer></v-spacer> {{ citeus }}
+              <strong> Project 1: </strong> <v-spacer></v-spacer>
+              {{ citeus }}
             </p>
           </v-card-subtitle>
           <v-card-actions class="justify-center">
             <v-btn outlined rounded to="/public">
               Explore
             </v-btn>
-          </v-card-actions></v-card
+          </v-card-actions>
+        </v-card>
+      </v-timeline-item>
+      <v-timeline-item>
+        <!-- Current event  -->
+        <span slot="opposite"> Current event </span>
+        <v-carousel
+          cycle
+          height="400"
+          hide-delimiter-background
+          show-arrows-on-hover
         >
-      </v-col>
-    </v-row>
-
-    <!-- Cite  -->
-    <v-row class="my-6">
-      <v-col lg="12">
-        <v-card outlined rounded="lg">
+          <v-carousel-item>
+            <v-img max-width="500" src="img/abstract_workflow.jpg"></v-img>
+          </v-carousel-item>
+        </v-carousel>
+      </v-timeline-item>
+      <!-- Citation here  -->
+      <v-timeline-item>
+        <span slot="opposite"> Interested us </span>
+        <v-card outlined rounded="lg" class="pa-2">
           <v-card-title class="justify-center">Cite us</v-card-title>
           <v-card-subtitle>
             <p class="text-h7">
@@ -116,22 +121,29 @@
             </p>
           </v-card-subtitle>
         </v-card>
-      </v-col>
-    </v-row>
+      </v-timeline-item>
+    </v-timeline>
 
-    <v-row>
-      <v-layout justify-center align-center>
-        <client-only>
-          <div class="viewer_options">
-            <script
-              id="clustrmaps"
-              type="text/javascript"
-              src="//cdn.clustrmaps.com/map_v2.js?d=y6edmNGXh1CPbChW0tW9Ahz11iiWoeE4HDc-yFfav5Y&cl=ffffff&w=a"
-            ></script>
-          </div>
-        </client-only>
-      </v-layout>
-    </v-row>
+    <div class="ma-5 pa-5">
+      <v-container>
+        <v-row>
+          <v-col lg="5" md="5" class="ml-md-auto"> </v-col>
+        </v-row>
+        <v-row>
+          <v-layout justify-center align-center>
+            <client-only>
+              <div class="viewer_options">
+                <script
+                  id="clustrmaps"
+                  type="text/javascript"
+                  src="//cdn.clustrmaps.com/map_v2.js?d=y6edmNGXh1CPbChW0tW9Ahz11iiWoeE4HDc-yFfav5Y&cl=ffffff&w=a"
+                ></script>
+              </div>
+            </client-only>
+          </v-layout>
+        </v-row>
+      </v-container>
+    </div>
   </v-container>
 </template>
 
@@ -173,5 +185,53 @@ export default {
 .viewer_options {
   width: 300px !important;
   max-width: 300px !important;
+}
+/*All h1 animate-pop-in css*/
+.animate-pop-in {
+  animation-delay: 0.8s;
+  animation: pop-in 1s cubic-bezier(0, 0.9, 0.3, 1.2) forwards;
+  opacity: 0;
+}
+@keyframes pop-in {
+  0% {
+    opacity: 0;
+    transform: translateY(-4rem) scale(0.8);
+  }
+  100% {
+    opacity: 1;
+    transform: none;
+  }
+}
+
+/*Subtitle typewriting anmination */
+.typewriter h2 {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: 0.15em solid orange; /* The typwriter cursor */
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 10 10; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: 0.05em; /* Adjust as needed */
+  animation-delay: 1.5s;
+  animation: typing 3.5s steps(40, end), blink-caret 0.75s step-end infinite;
+}
+
+/* The typing effect */
+@keyframes typing {
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from,
+  to {
+    border-color: transparent;
+  }
+  50% {
+    border-color: orange;
+  }
 }
 </style>
