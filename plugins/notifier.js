c446830fb3c57f9ffa9a7ae50ce8dcb05c1b5d94
Cankun Wang <flykun0620@gmail.com>
2021-03-04 19:50:20 -0500

    Add alert bar

--- plugins/notifier.js
+++ plugins/notifier.js
@@ -1,5 +1,8 @@
 export default ({ app, store }, inject) => {
   inject('notifier', {
+    showAlert({ content = '', color = '' }) {
+      store.commit('alert/showAlert', { content, color })
+    },
     showMessage({ content = '', color = '' }) {
       store.commit('snackbar/showMessage', { content, color })
     },
