c4baf59cfcc636dc4d3c3d421f8dd159f66e7fd2
Cankun Wang <flykun0620@gmail.com>
2021-04-10 18:08:06 -0400

    Add coverage plot

--- components/forms/CellClusterMultiome.vue
+++ components/forms/CellClusterMultiome.vue
@@ -728,149 +728,23 @@
                       <img :src="featureGene" :width="400" :height="350" />
                     </v-row> </grid-item
                 ></v-card>
-                <v-card class="ma-0"
-                  ><grid-item
-                    :x="layout[3].x"
-                    :y="layout[3].y"
-                    :w="layout[3].w"
-                    :h="layout[3].h"
-                    :i="layout[3].i"
-                    class="grid-item-border"
-                  >
-                    <v-card-title class="primary white--text caption px-2 py-1"
-                      >Gene set enrichment <v-spacer></v-spacer>
-                      <v-menu bottom left>
-                        <template v-slot:activator="{ on, attrs }">
-                          <v-btn dark icon v-bind="attrs" v-on="on">
-                            <v-icon>mdi-download-outline</v-icon>
-                          </v-btn>
-                        </template>
-
-                        <v-list>
-                          <v-list-item @click="downloadPDF">
-                            <v-list-item-title
-                              >Download Table</v-list-item-title
-                            >
-                          </v-list-item>
-                        </v-list>
-                      </v-menu></v-card-title
-                    >
-                    <v-row>
-                      <v-col cols="12"
-                        >Perform GSEA using the above DEGs
-                      </v-col>
-                      <v-col cols="12">
-                        <v-autocomplete
-                          v-model="gseaDatabase"
-                          class="ml-4"
-                          :items="allGseaDatabases"
-                          label="MSigDB database"
-                          return-object
-                          item-text="name"
-                          item-value="value"
-                        >
-                        </v-autocomplete> </v-col
-                    ></v-row>
-
-                    <v-row justify="center" class="mx-2 mb-2 mt-0">
-                      <v-btn
-                        class="mx-2 mb-2 mt-0"
-                        color="Primary"
-                        width="200"
-                        @click="runGSEA()"
-                        >Run</v-btn
-                      >
-                    </v-row>
-                    <div v-if="gseaResult.length">
-                      <v-data-table
-                        dense
-                        :headers="gseaHeaders"
-                        :items="gseaResult[0]"
-                        item-key="name"
-                        :items-per-page="5"
-                        class="elevation-1"
-                      ></v-data-table>
-                    </div> </grid-item
-                ></v-card>
+                <enrichment-table
+                  :deg="deResult"
+                  :x="layout[3].x"
+                  :y="layout[3].y"
+                  :w="layout[3].w"
+                  :h="layout[3].h"
+                  :i="layout[3].i"
+                ></enrichment-table>
+                <coverage-plot
+                  :genes="genes"
+                  :x="layout[4].x"
+                  :y="layout[4].y"
+                  :w="layout[4].w"
+                  :h="layout[4].h"
+                  :i="layout[4].i"
+                ></coverage-plot>
               </grid-layout>
-              <v-dialog v-model="addMetadataDialog" max-width="1200">
-                <v-card>
-                  <v-card-title>Annotate cell type</v-card-title>
-                  <v-divider class="my-2 py-2"></v-divider>
-                  <v-card-text>
-                    <v-row
-                      v-for="item in cellClusterArray"
-                      :key="item"
-                      class="my-0 py-0"
-                    >
-                      <v-col class="my-0 py-0" cols="4"
-                        >Cluster: {{ item.index }}
-                      </v-col>
-                      <v-col class="my-0 py-0" cols="6"
-                        >Cell type:
-                        <v-text-field
-                          v-model="newCellType[index]"
-                          outlined
-                          background-color="white"
-                          dense
-                        ></v-text-field
-                      ></v-col>
-                    </v-row>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn
-                      class="mx-2"
-                      color="primary"
-                      dark
-                      @click="addMetadata()"
-                    >
-                      Apply
-                    </v-btn>
-                    <v-btn
-                      color="grey darken-1"
-                      text
-                      @click="addMetadataDialog = false"
-                    >
-                      Cancel
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-              <v-dialog v-model="addTransferMetadataDialog" max-width="1200">
-                <v-card>
-                  <v-card-title
-                    >Reference based cell type annotation</v-card-title
-                  >
-                  <v-divider class="my-2 py-2"></v-divider>
-                  <v-card-text>
-                    <v-autocomplete
-                      v-model="currentAtlas"
-                      class="ml-4"
-                      :items="atlas"
-                      label="Select atlas data"
-                    ></v-autocomplete>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn
-                      class="mx-2"
-                      color="primary"
-                      dark
-                      @click="addReference()"
-                    >
-                      Apply
-                    </v-btn>
-                    <v-btn
-                      color="grey darken-1"
-                      text
-                      @click="addTransferMetadataDialog = false"
-                    >
-                      Cancel
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
             </div>
           </div>
         </v-col>
@@ -881,11 +755,16 @@
 <script>
 import _ from 'lodash'
 import ResizeImage from '~/components/utils/ResizeImage'
+import EnrichmentTable from '~/components/utils/EnrichmentTable'
+import CoveragePlot from '~/components/utils/CoveragePlot'
+
 import ApiService from '~/services/ApiService.js'
 
 export default {
   components: {
     scatter: ResizeImage,
+    'enrichment-table': EnrichmentTable,
+    'coverage-plot': CoveragePlot,
   },
   props: {
     type: { type: String, required: true, default: 'single_rna' },
@@ -922,7 +801,13 @@ export default {
         w: 2,
         h: 2,
         i: '3',
-        title: 'GSEA',
+      },
+      {
+        x: 0,
+        y: 4,
+        w: 2,
+        h: 2,
+        i: '4',
       },
     ],
     degList: [
@@ -1029,26 +914,6 @@ export default {
     // Renameing
     oldClusterName: '',
     newClusterName: '',
-    // GSEA
-    gseaHeaders: [
-      { text: 'pathway', value: 'pathway' },
-      { text: 'Adjusted p-value', value: 'padj' },
-      { text: 'NES', value: 'NES' },
-      { text: 'size', value: 'size' },
-      { text: '', value: 'data-table-expand' },
-    ],
-    gseaResult: [],
-    gseaDatabase: '',
-    allGseaDatabases: [
-      { name: 'Hallmark gene sets (H)', value: 'H' },
-      { name: 'Positional gene sets (C1)', value: 'C1' },
-      { name: 'Curated gene sets (C2)', value: 'C2' },
-      { name: 'Regulatory target gene sets (C3)', value: 'C3' },
-      { name: 'Computational gene sets (C4)', value: 'C4' },
-      { name: 'Ontology gene sets (C5)', value: 'C5' },
-      { name: 'Oncogenic signature gene sets (C6)', value: 'C6' },
-      { name: 'Immunologic signature gene sets (C7)', value: 'C7' },
-    ],
   }),
   computed: {
     identList() {
@@ -1667,26 +1532,7 @@ export default {
         color: 'accent',
       })
     },
-    async runGSEA() {
-      if (!this.deResult.length) {
-        this.$notifier.showMessage({
-          content: 'Please run DE testing first!',
-          color: 'error',
-        })
-      } else {
-        this.$notifier.showMessage({
-          content: 'Running GSEA...',
-          color: 'accent',
-        })
-        this.gseaResult = await ApiService.postCommand(
-          'deepmaps/api/queue/gsea-table/',
-          {
-            database: this.gseaDatabase.value,
-          }
-        )
-        console.log(this.gseaResult)
-      }
-    },
+
     async runGenePlot() {
       this.$notifier.showMessage({
         content: `Plotting ${this.gene} gene...`,
---
+++ components/utils/CoveragePlot.vue
@@ -0,0 +1,264 @@
+<template>
+  <v-card class="ma-0"
+    ><grid-item :w="w" :h="h" :x="x" :y="y" :i="i" class="grid-item-border">
+      <v-card-title class="primary white--text caption px-2 py-1"
+        >Coverage plot{{ type }} <v-spacer></v-spacer>
+        <v-menu bottom left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn dark icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-download-outline</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list>
+            <v-list-item @click="downloadPNG">
+              <v-list-item-title>Download PNG</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu></v-card-title
+      >
+
+      <v-row class="ma-4">
+        <v-col cols="12"
+          ><v-select
+            v-model="type"
+            :items="types"
+            label="Input type"
+            class="my-0"
+            item-text="name"
+            item-value="value"
+            @change="clearForm()"
+          ></v-select
+        ></v-col>
+
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-col cols="12">
+            <div
+              v-if="type === 'region'"
+              class="d-flex flex align-start justify-space-around"
+            >
+              <v-select
+                v-model="chr"
+                class="px-1"
+                :items="Chromosome"
+                outlined
+                dense
+                label="Chromosome"
+                :rules="[(v) => !!v || 'Item is required']"
+                background-color="white"
+              ></v-select>
+              <v-text-field
+                v-model="chrStart"
+                :rules="rangeRules"
+                class="px-1"
+                outlined
+                label="start"
+                type="number"
+                required
+                background-color="white"
+                dense
+              ></v-text-field>
+              <v-text-field
+                v-model="chrEnd"
+                :rules="[endRules]"
+                class="px-1"
+                outlined
+                label="end"
+                type="number"
+                required
+                background-color="white"
+                dense
+              ></v-text-field>
+            </div>
+            <div v-if="type === 'gene'" class="d-flex flex">
+              <v-autocomplete
+                v-model="gene"
+                class="ml-4"
+                :items="genes"
+                dense
+                outlined
+                :rules="[(v) => !!v || 'Item is required']"
+                label="Gene"
+              ></v-autocomplete>
+              <v-text-field
+                v-model="flanking"
+                class="px-1"
+                outlined
+                label="Flanking range"
+                type="number"
+                :rules="flankingRules"
+                required
+                background-color="white"
+                dense
+              ></v-text-field>
+            </div>
+          </v-col>
+        </v-form>
+      </v-row>
+
+      <v-row justify="center" class="mx-2 mb-2 mt-0">
+        <v-btn
+          class="mx-2 mb-2 mt-0"
+          color="Primary"
+          width="200"
+          @click="runCoveragePlotStatic()"
+          >Run</v-btn
+        >
+      </v-row>
+      <div v-if="src">
+        <img :src="src" :width="imagew" :height="imageh" />
+      </div> </grid-item
+  ></v-card>
+</template>
+
+<script>
+import ApiService from '~/services/ApiService.js'
+
+export default {
+  props: {
+    genes: { type: Array || String, required: true },
+    w: { type: Number, required: true, default: 2 },
+    h: { type: Number, required: true, default: 2 },
+    x: { type: Number, required: true, default: 0 },
+    y: { type: Number, required: true, default: 0 },
+    i: { type: String, required: true, default: '0' },
+    imagew: { type: Number, required: false, default: 500 },
+    imageh: { type: Number, required: false, default: 300 },
+  },
+
+  data() {
+    return {
+      valid: true,
+      types: [
+        { name: 'Search by gene', value: 'gene' },
+        { name: 'Specify genomic regions', value: 'region' },
+      ],
+      Chromosome: [
+        'chr1',
+        'chr2',
+        'chr3',
+        'chr4',
+        'chr5',
+        'chr6',
+        'chr7',
+        'chr8',
+        'chr9',
+        'chr10',
+        'chr11',
+        'chr12',
+        'chr13',
+        'chr14',
+        'chr15',
+        'chr16',
+        'chr17',
+        'chr18',
+        'chr19',
+        'chr20',
+        'chr21',
+        'chr22',
+        'chrX',
+        'chrY',
+      ],
+      type: 'gene',
+      gene: NaN,
+      chr: '',
+      chrStart: '',
+      chrEnd: '',
+      src: '',
+      flanking: 2000,
+      isAnnotation: true,
+      isPeak: false,
+      // Form rules
+      flankingRules: [
+        (v) => !!v || 'Flanking region is required',
+        (v) => (v && v >= 0) || 'Flanking region be positive',
+      ],
+      rangeRules: [
+        (v) => !!v || 'Range is required',
+        (v) => (v && v > 0) || 'Range should be greater than 0',
+      ],
+    }
+  },
+  computed: {},
+  methods: {
+    clearForm() {
+      this.$refs.form.reset()
+    },
+    downloadPNG() {
+      const link = document.createElement('a')
+      link.href = this.src
+      link.setAttribute('download', 'Image_png.png')
+      document.body.appendChild(link)
+      link.click()
+    },
+    async runCoveragePlotStatic() {
+      if (!this.$refs.form.validate()) {
+        this.$notifier.showMessage({
+          content: 'Please check your inputs.',
+          color: 'error',
+        })
+        this.$refs.form.resetValidation()
+        return
+      }
+      if (this.type === 'gene' && !this.gene) {
+        this.$notifier.showMessage({
+          content: 'Please specify gene name!',
+          color: 'error',
+        })
+      } else {
+        this.src = await ApiService.postCommand(
+          'deepmaps/api/queue/coverage-plot/',
+          {
+            type: this.type,
+            gene: this.gene,
+            flank: this.flanking,
+            chr: this.chr,
+            start: this.chrStart,
+            end: this.chrEnd,
+            is_annotation: true,
+            is_peak: false,
+          }
+        )
+      }
+    },
+    endRules(v) {
+      if (v - this.chrStart < 100) {
+        return 'End should be at least 100 bps larger than start'
+      }
+      if (!v) {
+        return 'Range is required'
+      }
+      if (v <= 0) {
+        return 'Range should be greater than 0'
+      }
+      return true
+    },
+    resizeEvent(i, newH, newW, newHPx, newWPx) {
+      console.log(
+        'RESIZE i=' +
+          i +
+          ', H=' +
+          newH +
+          ', W=' +
+          newW +
+          ', H(px)=' +
+          newHPx +
+          ', W(px)=' +
+          newWPx
+      )
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.grid-item-border {
+  border: 1px solid #d3d3d3;
+  flex-direction: column;
+  position: relative;
+  display: flex;
+  height: 100%;
+  border-radius: 4px;
+  background: white;
+}
+</style>
