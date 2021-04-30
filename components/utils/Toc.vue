f135120d020af27413f8ebe6b6f7dd21fdf28958
Cankun Wang <flykun0620@gmail.com>
2021-04-29 21:04:23 -0400

    Add toc

---
+++ components/utils/Toc.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-navigation-drawer
+    class="grey lighten-4"
+    app
+    right
+    clipped
+    fixed
+    width="350"
+    ><div v-html="content"></div>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Toc',
+  props: {
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
