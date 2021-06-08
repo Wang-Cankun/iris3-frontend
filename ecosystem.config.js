097212b749cb37608799367827cdde9ae4bc123e
Cankun Wang <flykun0620@gmail.com>
2021-06-08 00:14:56 -0400

    remove test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,18 +12,5 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
-    {
-      name: 'DeepMAPS-frontend-test',
-      script: './node_modules/nuxt-start/bin/nuxt-start.js',
-      port: 9006,
-      watch: '.',
-      args: 'start -H 0.0.0.0',
-      max_memory_restart: '16G',
-      log_date_format: 'YYYY-MM=DD HH:mm:ss',
-      env: {
-        NODE_ENV: 'production',
-        BASE_URL: 'deepmaps_test',
-      },
-    },
   ],
 }
